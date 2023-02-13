b292ae4ccbe13bd3ce1fb05dd4df3ba63cf8c646
Satish Venkatakrishnan <satish860@gmail.com>
2023-02-13 14:12:37 +0530

    Scrapper Pipeline with ID

--- src/Pitchcast.Scrapper/Program.cs
+++ src/Pitchcast.Scrapper/Program.cs
@@ -8,6 +8,10 @@
             foreach (var item in scrapperPipeline.GetAllGenre())
             {
                 Console.WriteLine($"Got the Genre of {item.Name} with Url {item.Link}");
+                foreach (var podcast in scrapperPipeline.GetPopularPodcast(item.Link))
+                {
+                    Console.WriteLine($"  Popular Podcast in {item.Name} with {podcast.Id} and {podcast.Name}");
+                } 
             } 
         }
     }
--- src/Pitchcast.Scrapper/ScrapperPipeline.cs
+++ src/Pitchcast.Scrapper/ScrapperPipeline.cs
@@ -2,6 +2,7 @@
 using HtmlAgilityPack.CssSelectors.NetCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,7 +27,53 @@ namespace Pitchcast.Scrapper
             }).ToList();
             return genreList;
         }
+
+        public List<Podcast> GetPopularPodcast(string url)
+        {
+            var web = new HtmlWeb();
+            var indivdualGenre = web.Load(url);
+            List<Podcast> podcasts = new List<Podcast>();
+            foreach (var node in indivdualGenre.QuerySelectorAll("div > div"))
+            {
+                if (node.Id.Equals("selectedcontent", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    var links = node.QuerySelectorAll("a");
+                    podcasts = links.Select(p =>
+                    {
+                        return new Podcast
+                        {
+                            Name = p.InnerHtml,
+                            Url = p.Attributes["href"].Value,
+                            Id = GetPodcastId(p.Attributes["href"].Value)
+
+                        };
+                    }).ToList();
+                }
+            }
+            return podcasts;
+        }
+
+        
+
+        public string GetPodcastId(string url)
+        {
+            var idfragment = url.Split("/").Last().Replace("id", "");
+            return idfragment;
+        }
     }
+
+
+    public class Podcast
+    {
+        public string Name { get; set; }
+
+        public string Id { get; set; }
+
+        public string Url { get; set; }
+    }
+
+
+   
 
     public class Genre
     {
