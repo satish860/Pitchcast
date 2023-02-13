4e8866818294971c60fe1d121939c906e5dfab96
Satish Venkatakrishnan <satish860@gmail.com>
2023-02-13 10:24:07 +0530

    Initial Commit for Solution and project.

---
+++ src/Pitchcast.Scrapper/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Pitchcast.Scrapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
