2fe7a757f7423abd2fc9dfa949e927db477a9cdb
Satish Venkatakrishnan <satish860@gmail.com>
2023-02-16 09:54:29 +0530

    Now I have 4.5K documents of Podcast with me. Isnt that great

--- src/Pitchcast.Scrapper/Program.cs
+++ src/Pitchcast.Scrapper/Program.cs
@@ -35,42 +35,19 @@ namespace Pitchcast.Scrapper
                 Console.WriteLine($"Got the Genre of {item.Name} with Url {item.Link}");
                 var ids = ScrapperPipeline
                     .GetPopularPodcast(item.Link)
-                    .Select(p => p.Id).First();
-
-
-                await Publisher.PublishAsync("hello", ids).ConfigureAwait(false);
-                
-               
-                Console.ReadKey();
-
-
-                //foreach (var pagedIds in ids.Page(200))
-                //{
-                //    var stringSeperatedIds = string.Join(",", pagedIds);
-
-                //    var details = await ScrapperPipeline.GetPodCastDetails(stringSeperatedIds).ConfigureAwait(false);
-                //    podcasts.AddRange(details);
-                //}
+                    .Select(p => p.Id);
+                Console.WriteLine($"There are {ids.Count()} of Podcast");
+                var i = 0;
+                foreach (var id in ids)
+                {
+                    i++;
+                    await Publisher.PublishAsync("hello", id).ConfigureAwait(false);
+                    Console.WriteLine($"I am in {i} in of the total {ids.Count()}");
+                    Thread.Sleep(500);
+                }
             }
 
-            //List<PodcastEpisode> podcastedpisode = new List<PodcastEpisode>();
-            //Console.WriteLine($"Total Podcast in popular category {podcasts.Count}");
-            //var count = 0;
-            //foreach (var podcast in podcasts)
-            //{
-            //    Console.WriteLine($"I am in {count} out of {podcasts.Count}");
-            //    if (podcast.FeedUrl != null)
-            //    {
-            //        var episode = await ScrapperPipeline
-            //                            .GetPodcastEpisodesAsync(podcast.FeedUrl.OriginalString)
-            //                            .ConfigureAwait(false);
-            //        podcastedpisode.AddRange(episode);
-            //    }
-
-            //    count++;
-            //}
-            //var JsonList = JsonSerializer.Serialize(podcastedpisode);
-            //await File.WriteAllTextAsync("someepisode.json", JsonList).ConfigureAwait(false);
+          
         }
 
 
